69bbf4985ab36525e15f9cc674b20e7adbbcf96a
Jaycie Kassahn <jckassahn@gmail.com>
2020-08-08 16:59:15 -0700

    Serve static files for every product page

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
+const path = require('path');
 const express = require('express');
+
 const app = express();
 const port = 3013;
 const db = require('../db/index.js');
 
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/:productId', (req, res) => {
-  console.log('product id', req.params.productId);
+app.use('/:productId', express.static(path.join(__dirname, '../public')));
+
+app.get('/video/:productId', (req, res) => {
   db.Video.find({ productId: req.params.productId }, (err, videoData) => {
     if (err) {
       console.error('Error retrieving video data', err);
