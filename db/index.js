fe9f769da0a19919e299539a63d2d2bc0dfd4331
Jaycie Kassahn <jckassahn@gmail.com>
2020-08-07 21:31:07 -0700

    Remove unnecessary function

--- db/index.js
+++ db/index.js
@@ -16,21 +16,4 @@ const videoSchema = new Schema({
 
 const Video = mongoose.model('Video', videoSchema);
 
-let addVideo = (videoObj) => {
-  let { title, author, duration, url, thumbnail, likes, dislikes, productId, category } = videoObj;
-
-  let newVideo = new Video({
-    title: title,
-    author: author,
-    duration: duration,
-    url: url,
-    thumbnail: thumbnail,
-    likes: likes,
-    dislikes: dislikes,
-    productId: productId,
-    category: category
-  });
-  newVideo.save();
-};
-
-module.exports.addVideo = addVideo;
+module.exports.Video = Video;
