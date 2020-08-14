44864b257e6f4b8835474c255d73373b41056f9c
Jaycie Kassahn <jckassahn@gmail.com>
2020-08-13 20:06:53 -0700

    Prevent clicks and scrolling on page while modal is open

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import CarouselCard from './carousel-card.jsx';
 import VideoModal from './video-modal.jsx';
+import classes from './carousel.module.css';
 
 export default class VideoCarousel extends React.Component {
   constructor(props) {
@@ -31,26 +32,22 @@ export default class VideoCarousel extends React.Component {
 
   handleCardClick(event) {
     const { videoData } = this.state;
-    for (let i = 0; i < videoData.length; i += 1) {
-      if (event.target.id === videoData[i]._id) {
-        this.setState({
-          selectedVideo: videoData[i],
-        });
-      }
-    }
+    this.setState({
+      selectedVideo: videoData[event.target.id],
+    });
+    document.body.className = classes.modalOpen;
   }
 
   handleModalClose() {
     this.setState({
       selectedVideo: null,
     });
+    document.body.className = '';
   }
 
   render() {
     const { videoData, selectedVideo } = this.state;
-    const generateCards = (video) => {
-      return <CarouselCard key={video._id} cardId={video._id} video={video} handleClick={this.handleCardClick} />
-    };
+    const generateCards = (video, index) => <CarouselCard key={video._id} cardId={index} video={video} handleClick={this.handleCardClick} />;
     const cards = videoData.map(generateCards);
     return (
       <div>
